8ab2f5b6a6f2d50a70cedd987a8f8f23601f8c3b
Padmavati08 <padmavatinaik182@gmail.com>
2026-07-10 00:15:46 -0700

    feat: add PDF loading and text extraction

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("RAG project setup")
+from langchain_community.document_loaders import PyPDFLoader
+
+# Path to your PDF
+file_path = "data/notes.pdf"
+
+# Load PDF
+loader = PyPDFLoader(file_path)
+documents = loader.load()
+
+# Print number of pages
+print("Total pages:", len(documents))
+
+# Print first page content
+print("\n--- First Page Content ---\n")
+print(documents[0].page_content)
